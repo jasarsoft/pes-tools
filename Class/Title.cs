5eab9d424bfffd81f43f0e7955fc08d12f75637b
Edin Jašarević <edinjasar14@gmail.com>
2017-06-23 16:06:41 +0200

    create title class

---
+++ Class/Title.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Jasarsoft.Multiplayer
+{
+    private class Title
+    {
+        private readonly string titleInfo;
+        private readonly string titleError;
+        private readonly string titleWarning;
+
+        public Title()
+        {
+            About about = new About();
+
+            this.titleInfo = about.Title + " | Information";
+            this.titleError = about.Title + " | Error";
+            this.titleWarning = about.Title + " | Warning";
+        }
+
+        public string Info
+        {
+            get { return this.titleInfo; }
+        }
+
+        public string Error
+        {
+            get { return this.titleError; }
+        }
+
+        public string Warning
+        {
+            get { return this.titleWarning; }
+        }
+    }
+}
