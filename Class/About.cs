e61bf2953d710c8d641f3e367b41d072c8f2126e
Edin Jašarević <edinjasar14@gmail.com>
2017-06-23 16:07:29 +0200

    create about class

---
+++ Class/About.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.Multiplayer
+{
+    private class About
+    {
+        public string Name
+        {
+            get { return "kithost.exe"; }
+        }
+
+        public string Title
+        {
+            get { return "PES6 Tools: Multiplayer"; }
+        }
+
+        public string License
+        {
+            get { return "Apache License v2.0"; }
+        }
+
+        public string Website
+        {
+            get { return "www.github.com/jasarsoft/"; }
+        }
+
+        public string Developer
+        {
+            get { return "Jasarsoft"; }
+        }
+
+        public string Description
+        {
+            get { return "Server address control tool in the system host file."; }
+        }
+    }
+}
