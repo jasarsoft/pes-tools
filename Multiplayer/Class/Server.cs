0807aa61afc91cf4ff8acb3a1ea792859fc6a256
Edin Jašarević <edinjasar14@gmail.com>
2017-06-24 08:21:12 +0200

    create server class

---
+++ Multiplayer/Class/Server.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Jasarsoft.Multiplayer
+{
+    static internal class Server
+    {
+        private static string name;
+        private static string address;
+        private static string comment;
+
+        public Server()
+        {
+            name = null;
+            address = null;
+            comment = null;
+        }
+
+
+        public static string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public static string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public static string Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+
+
+        public static void Default()
+        {
+            ServerDefault sd = new ServerDefault();
+
+            name = sd.Name;
+            address = sd.Address;
+            comment = sd.Comment;
+        }
+    }
+}
