4193f069c2b61550cd51a28e89075fcc28c6ec1f
Edin Jašarević <edinjasar14@gmail.com>
2017-06-24 08:08:51 +0200

    new organization resource

---
+++ Multiplayer/Class/ServerDefault.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Jasarsoft.Multiplayer
+{
+    internal class ServerDefault
+    {
+        private readonly string name;
+        private readonly string address;
+        private readonly string comment;
+
+
+        public ServerDefault()
+        {
+            this.name = "localhost";
+            this.address = "127.0.0.1";
+            this.comment = "default host";
+        }
+
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public string Address
+        {
+            get { return this.address; }
+        }
+
+        public string Comment
+        {
+            get { return this.comment; }
+        }
+    }
+}
